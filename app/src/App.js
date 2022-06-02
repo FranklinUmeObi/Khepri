ed99afb2ae2fe35058327282c1bd1db11d13a277
Franklin Ume Obiekwe <franklinumeobi@gmail.com>
2022-06-02 17:17:24 +0100

    Set up Routing

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+import HomePage from "./Pages/HomePage/HomePage";
+import DonatePage from "./Pages/DonatePage/DonatePage";
+import AboutPage from "./Pages/AboutPage/AboutPage";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="donate" element={<DonatePage />} />
+        <Route path="about" element={<AboutPage />} />
+      </Routes>
     </div>
   );
 }
